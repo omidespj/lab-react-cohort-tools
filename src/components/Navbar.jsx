1011655b22f05ae1efcf877484cc47ad96456adf
Omid Jahromi <omid.esmaeelipoor@gmail.com>
2024-07-07 03:51:36 +0200

    solved lab

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import logo from "./../assets/logo-ironhack-blue.png";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
@@ -8,7 +9,9 @@ function Navbar() {
 
           {/* Home Button (Logo) */}
           <button className="flex items-center text-l py-1">
+            <Link to="/">
             <img src={logo} alt="Logo" className="h-8 w-auto" />
+            </Link>
           </button>
         </div>
 
@@ -21,11 +24,13 @@ function Navbar() {
         <div className="w-1/4 flex justify-end mr-4">
           {/* User Profile Button */}
           <button className="flex items-center text-l py-1">
+            <Link to="/profile">
             <img
               src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/lab-cohort-tools-routing/profile-1.png"
               alt="Logo"
               className="h-10 w-auto border-solid border border-white rounded-3xl p-1"
             />
+            </Link>
           </button>
 
         </div>
